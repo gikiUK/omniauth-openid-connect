bfee07598c86eb5ce6b5837e15bb1bf8b128d6c2
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2024-10-11 13:59:06 +0100

    Added binding.pry

--- lib/omniauth/strategies/openid_connect.rb
+++ lib/omniauth/strategies/openid_connect.rb
@@ -612,6 +612,8 @@ module OmniAuth
       def encoded_post_logout_redirect_uri
         return nil unless options.post_logout_redirect_uri
 
+        binding.pry
+
         # post_logout_redirect_uri を指定する場合は, id_token_hint 必須.
         URI.encode_www_form(
           id_token_hint: params['id_token'],
