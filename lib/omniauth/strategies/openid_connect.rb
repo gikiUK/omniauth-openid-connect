ffcba5a2c83a0db5c798a4e4c2b4f9868e2f14d1
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2024-10-11 13:56:26 +0100

    Trying to use the params object

--- lib/omniauth/strategies/openid_connect.rb
+++ lib/omniauth/strategies/openid_connect.rb
@@ -614,7 +614,7 @@ module OmniAuth
 
         # post_logout_redirect_uri を指定する場合は, id_token_hint 必須.
         URI.encode_www_form(
-          id_token_hint: @access_token.token_id,
+          id_token_hint: params['id_token'],
           post_logout_redirect_uri: options.post_logout_redirect_uri
         )
       end
